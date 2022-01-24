469ac5a9b4a44b8cbb33b62ed7be23c666f78887
Dryice Liu <dryiceliu@gmail.com>
2022-01-24 16:29:30 +0800

    use  Django 1.8

--- celery_progress/urls.py
+++ celery_progress/urls.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.conf.urls import url
 from . import views
 
 app_name = 'celery_progress'
 urlpatterns = [
-    re_path(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
+    url(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
 ]
